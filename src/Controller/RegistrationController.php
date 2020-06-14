b237fcd83f1f23c257a76cf7f8e522c3baa75238
fred34560 <mega-services@hotmail.fr>
2020-06-14 16:10:15 +0200

    mise en place de l'activation + button login + logout en francais

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -77,18 +77,19 @@ class RegistrationController extends AbstractController
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
+
         // validate email confirmation link, sets User::isVerified=true and persists
         try {
             $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $exception->getReason());
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('clients_home');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('clients_home');
     }
 }
